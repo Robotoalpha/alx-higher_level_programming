2ffe8855a0d0adf5f895b854315f8d9bb490bf3a
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-06-06 08:34:14 +0000

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # 5-variable_load.py
-# Ouattara Alphonse <ujlog01@gmail.com>
+# Gedeon Obae Gekonge <gideonobae@gmail.com.com>
 
 if __name__ == "__main__":
     """Print the value of variable a from variable_load_5."""
-    
     from variable_load_5 import a
+
     print(a)
