baae66cfd3b390ec8d7e657ecf853e46b718931f
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-06-08 14:29:37 +0000

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 # 12-roman_to_int.py
-# Ouattara Alphonse <ujlog01@.com>
+# Brennan D Baraban <375@holbertonschool.com>
+
 
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
+
     roman_dict = {
             "I": 1,
             "V": 5,
@@ -21,9 +23,11 @@ def roman_to_int(roman_string):
     for i in range(len(roman_string)):
         if roman_dict.get(roman_string[i], 0) == 0:
             return (0)
+
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
                 num += roman_dict[roman_string[i]] * -1
+
         else:
             num += roman_dict[roman_string[i]]
     return (num)
