a6b2ee7ce19456f2043bbf30646f9497676b3cfe
Robotoalpha <99337731+Robotoalpha@users.noreply.github.com>
2022-06-08 13:36:30 +0000

    a function that replaces or adds key/value in a dictionary.

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,2 +1,8 @@
+#!/usr/bin/python3
+# 7-update_dictionary.py
+# Ouattara Alphonse <ujlog01@gmail.com>
 
-
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
